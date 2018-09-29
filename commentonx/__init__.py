5f3c77a30e489ec8bd3b0fd7903730afa7d8a2b3
Dan Trickey <dan@trickey.io>
2018-09-29 17:28:49 +0100

    Convert to use SCSS

--- commentonx/__init__.py
+++ commentonx/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_scss import Scss
 
 from commentonx.config import config
 
@@ -10,5 +11,7 @@ if 'VIEW_CONFIG' in app.config:
     app.jinja_env.globals['VIEW_CONFIG'] = app.config['VIEW_CONFIG']  # Allow view config access in templates
 else:
     app.jinja_env.globals['VIEW_CONFIG'] = {}
+
+Scss(app)
 
 from commentonx import views
