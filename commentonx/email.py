772a11fe7ccfb1428baee1cf57a3d04e0ddfd271
Dan Trickey <dan@trickey.io>
2018-09-29 18:46:06 +0100

    Add HTML Template to email

--- commentonx/email.py
+++ commentonx/email.py
@@ -1,4 +1,5 @@
 from commentonx import app
+from flask import render_template
 
 def send_feedback(feedback):
 
@@ -7,6 +8,6 @@ def send_feedback(feedback):
     to_address = app.config["MAIL_TO"]
     from_address = app.config["MAIL_FROM"]
     subject = app.config["MAIL_SUBJECT"]
-    content = feedback
+    content = render_template("email.html", feedback=feedback)
 
     send_email(to_address, from_address, subject, content)
